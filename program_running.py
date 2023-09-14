ee1d79cfac12e816df965efaeb2369a117260356
matthew2848123 <repecki88@gmail.com>
2023-09-14 10:24:50 -0400

    Added in the .exe in dist

---
+++ program_running.py
@@ -0,0 +1,26 @@
+import psutil
+
+bad_programs = ['wireshark','cmd','vim','terminal','bash']
+def check_if_process_running(process_name,kill = False):
+    '''
+    :param process_name: Takes in process name
+    :param kill: Boolean of to kill the process or not
+    :return: Returns 0 if process not found and not killed, returns 1 if process is found but not killed, returns 2 if process is found and killed
+    '''
+    ret = 0
+    for process in psutil.process_iter(['name']):
+        print(process)
+        if process_name in process.info['name'] or process.info['name'] in process_name :
+            if kill:
+                process.kill()
+                ret += 1
+            ret += 1
+    return ret
+
+def kill_bad_programs():
+    while True:
+        for program in bad_programs:
+            print(check_if_process_running(program,True))
+
+kill_bad_programs()
+
